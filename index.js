8b4793dffe02b4ebf10e1c41603ef65515aefbe9
KanishkMishra143 <kanishk.mishra012@adgitmdelhi.ac.in>
2026-05-18 16:12:15 +0530

    Added dynamic env port to index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
   res.send('<h1>🚀 Hello from AutoDeploy Node.js!</h1><p>Environment: ' + JSON.stringify(process.env) + '</p>');
